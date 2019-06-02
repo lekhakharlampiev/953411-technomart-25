319fada1a64f761aa1e983eeef753c00e48429c5
Aleksey <xarlamlexa1502@gmail.com>
2019-06-02 22:58:49 +0300

    экспортировал картинки из блока преимуществ на главной странице. Доработал скрипт, добавив цикл который добавляет обработчик на каждую кнопку карточки товара.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,22 @@
 //открывается по нажатию на кнопку "купить" в карточке товара
 var popupInCatalog = document.querySelector('.js-popup-catalog');
 var popupCatalogClose = document.querySelector('.js-close-popup-catalog');
-var toBuyProductButton = document.querySelector('.js-card-product-buy');
+var toBuyProductButton = document.querySelectorAll('.js-card-product-buy');
+var returnShoppingButton = document.querySelector('.js-return-shopping');
 
-toBuyProductButton.addEventListener('click', function (evt) {
+for (var i = 0; i < toBuyProductButton.length; i++) {
+	toBuyProductButton[i].addEventListener('click', function (evt) {
 	evt.preventDefault();
 	popupInCatalog.classList.add('popup-show');
 });
+}
 
 popupCatalogClose.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	popupInCatalog.classList.remove('popup-show');
+});
+
+returnShoppingButton.addEventListener('click', function (evt) {
 	evt.preventDefault();
 	popupInCatalog.classList.remove('popup-show');
 });
