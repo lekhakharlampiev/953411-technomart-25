3c2241af1dd294604983a3608aeb6bda951c29d6
Aleksey <kharlampiev@aspect-systems.ru>
2019-06-05 13:34:24 +0300

    Оживил слайдер товаров

--- js/index.js
+++ js/index.js
@@ -28,3 +28,63 @@ fullMapClose.addEventListener('click', function (evt) {
     evt.preventDefault();
     fullMap.classList.remove('popup-show');
 })
+// слайдер товаров
+var slider = document.querySelectorAll(".js-slider-product");
+var nextSlider = document.querySelector(".js-next-slider");
+var previousSlider = document.querySelector(".js-previous-slider");
+
+nextSlider.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	slider[nextSliderIndex(slider)].classList.add("show-slider");
+
+})
+previousSlider.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	slider[previousSliderIndex(slider)].classList.add("show-slider");
+
+})
+//функция переключения слайдеров вперед по кругу
+var nextSliderIndex = function(slider) {
+	var start;
+	//находим слайдер, который включен
+	for(i = 0; i < slider.length; i++) {
+		if(slider[i].classList.contains("show-slider")) {
+			start = i;
+		}
+	}
+	//отключаем активный слайдер
+	slider[start].classList.remove("show-slider");
+	var next;
+	//если слайдер был последний, возвращаемся к первому,
+	//если нет - переходим к следующему
+	if (start === slider.length - 1) {
+		next = 0;
+	} else {
+		next = start + 1;
+	}
+
+	return next;
+}
+//функция переключения слайдера назад по кругу
+var previousSliderIndex = function(slider) {
+	var start;
+	//находим слайдер, который включен
+	for(i = 0; i < slider.length; i++) {
+		if(slider[i].classList.contains("show-slider")) {
+			start = i;
+		}
+	}
+	//отключаем активный слайдер
+	slider[start].classList.remove("show-slider");
+	var next;
+	//если слайдер был последний, возвращаемся к первому,
+	//если нет - переходим к следующему
+	if (start === 0) {
+		next = slider.length - 1;
+	} else {
+		next = start - 1;
+	}
+
+	return next;
+}
+//конец слайдера товаров
