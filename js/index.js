43fda12228badda9bdf5dc655c5148234cb86cfb
Aleksey <xarlamlexa1502@gmail.com>
2019-06-09 19:38:16 +0300

    Объединил js-код в один документ. Добавил переключение слайдера Услуг.

--- js/index.js
+++ js/index.js
@@ -1,48 +1,80 @@
+//модальное окно каталога
+//открывается по нажатию на кнопку "купить" в карточке товара
+var popupInCatalog = document.querySelector('.js-popup-catalog');
+var popupCatalogClose = document.querySelector('.js-close-popup-catalog');
+var toBuyProductButton = document.querySelectorAll('.js-card-product-buy');
+var returnShoppingButton = document.querySelector('.js-return-shopping');
+
+for (var i = 0; i < toBuyProductButton.length; i++) {
+    toBuyProductButton[i].addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	popupInCatalog.classList.add('popup-show');
+	});
+}
+
+if (popupCatalogClose) {
+	popupCatalogClose.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	popupInCatalog.classList.remove('popup-show');
+	});
+
+	returnShoppingButton.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	popupInCatalog.classList.remove('popup-show');
+	});
+}
 
 //модальное окно обратной связи 
 var popupFeedBack = document.querySelector('.js-popup-feedback');
 var popupFeedBackOpen = document.querySelector('.js-button-main-contacts');
 var popupFeedBackClose = document.querySelector('.js-close-popup-feedback');
 
-popupFeedBackOpen.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popupFeedBack.classList.add('popup-show');
-})
+if (popupFeedBackOpen && popupFeedBack && popupFeedBackClose) {
+	popupFeedBackOpen.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	popupFeedBack.classList.add('popup-show');
+	});
 
-popupFeedBackClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popupFeedBack.classList.remove('popup-show');
-})
+	popupFeedBackClose.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	popupFeedBack.classList.remove('popup-show');
+	});
+}
+
 // большая карта
 
 var smallMap = document.querySelector('.js-min-map');
 var fullMap = document.querySelector('.js-full-map');
 var fullMapClose = document.querySelector('.js-close-map');
 
-smallMap.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    fullMap.classList.add('popup-show');
-})
+if (smallMap && fullMap && fullMapClose) {
+	smallMap.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	fullMap.classList.add('popup-show');
+	})
 
-fullMapClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    fullMap.classList.remove('popup-show');
-})
+	fullMapClose.addEventListener('click', function (evt) {
+    	evt.preventDefault();
+    	fullMap.classList.remove('popup-show');
+	})
+}
+
 // слайдер товаров
 var slider = document.querySelectorAll(".js-slider-product");
 var nextSlider = document.querySelector(".js-next-slider");
 var previousSlider = document.querySelector(".js-previous-slider");
+if (slider && nextSlider && previousSlider) {
+	nextSlider.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		slider[nextSliderIndex(slider)].classList.add("show-slider");
 
-nextSlider.addEventListener('click', function(evt) {
-	evt.preventDefault();
-	slider[nextSliderIndex(slider)].classList.add("show-slider");
+	});
+	previousSlider.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		slider[previousSliderIndex(slider)].classList.add("show-slider");
 
-})
-previousSlider.addEventListener('click', function(evt) {
-	evt.preventDefault();
-	slider[previousSliderIndex(slider)].classList.add("show-slider");
-
-})
+	});
+}
 //функция переключения слайдеров вперед по кругу
 var nextSliderIndex = function(slider) {
 	var start;
@@ -89,3 +121,21 @@ var previousSliderIndex = function(slider) {
 }
 //конец слайдера товаров
 //слайдер сервисов
+var serviseSliderButtons = document.querySelectorAll(".js-additional-service-button");
+var serviseSlider = document.querySelectorAll(".js-service-slider");
+for (var i = 0; i < serviseSliderButtons.length; i++) {
+	serviseSliderButtons[i].addEventListener('click', function(evt) {
+		evt.preventDefault();
+		var numberClickElement;
+		for (var j = 0; j < serviseSliderButtons.length; j++) {
+			if (this === serviseSliderButtons[j]) {
+				numberClickElement = j;
+			}
+			serviseSlider[j].classList.remove('select-slider');
+			serviseSliderButtons[j].classList.remove('service-select');
+
+		}
+		this.classList.add('service-select');
+		serviseSlider[numberClickElement].classList.add('select-slider');
+	})
+}
